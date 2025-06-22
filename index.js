8b51acebf3696b6d740f61c7534c842a06e7977e
mumtaza <mumtazmohamed453@gmail.com>
2025-06-22 13:33:54 +0300

    Complete arrow function lab and pass all tests

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = function(num1, num2) {
+  return num1 / num2;
+};
+const square = (number) => {
+  return number * number;
+};
+const add = (num1, num2) => {
+  return num1 + num2;
+};
